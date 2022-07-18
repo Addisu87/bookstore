afe89eea8448675380855d8763b75875b1203ac7
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-07-18 11:20:54 +0300

    Modify the form

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,33 @@
+import { Autocomplete, Button, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import styled from 'styled-components';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/ThunkFunctions';
+import '../styles/addBook.css';
+
+const categories = [
+  'Computer Programming',
+  'Action',
+  'Adventure',
+  'Fantasy',
+  'Horror',
+  'Classic',
+  'History',
+  'Detective and Mystery',
+  'Romance',
+  'Sci-Fi',
+  'Humor',
+  'Tragedy',
+  'Documentary',
+];
 
 const AddBook = () => {
   const [inputText, setInputText] = useState({
     title: '',
     author: '',
   });
+  const [categoryName, setCategoryName] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
   const dispatch = useDispatch();
 
@@ -23,7 +42,7 @@ const AddBook = () => {
     item_id: uuidv4(),
     title: inputText.title,
     author: inputText.author,
-    category: 'Fiction',
+    category: categoryName,
   };
 
   const handleAddBook = () => {
@@ -38,101 +57,68 @@ const AddBook = () => {
   };
 
   return (
-    <FormContainer>
-      <Title>Add new book</Title>
-      <Form onSubmit={handleSubmit}>
-        <Input
+    <form onSubmit={handleSubmit}>
+      <div className="componentDivider" />
+      <h2 className="addBookTitle">Add new book</h2>
+      <div className="inputsContainer">
+        <TextField
+          required
+          id="outlined-size-small"
+          size="small"
+          label="Title"
+          variant="outlined"
           type="text"
           name="title"
           placeholder="Book title"
-          id="title"
           value={inputText.title}
           onChange={handleInputChange}
-          required
+          className="inputs"
         />
-        <Input
+        <TextField
+          required
+          id="outlined-size-small"
+          size="small"
+          label="Author"
+          variant="outlined"
           type="text"
           name="author"
           placeholder="Author"
-          id="author"
           value={inputText.author}
           onChange={handleInputChange}
-          required
+          className="inputs"
         />
+        <div>
+          <Autocomplete
+            id="outlined-size-small"
+            size="small"
+            value={categoryName}
+            onChange={(event, newValue) => {
+              setCategoryName(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            options={categories}
+            sx={{ width: 150 }}
+            /* eslint-disable */
+            renderInput={(params) => (
+              <TextField {...params} placeholder="Category" />
+            )}
+            /* eslint-enable */
+          />
+        </div>
         <Button
+          variant="contained"
           type="submit"
           onClick={handleAddBook}
-          className="btn btn-success"
+          className="addBookBtn"
         >
           ADD BOOK
         </Button>
-      </Form>
-    </FormContainer>
+      </div>
+    </form>
   );
 };
 
 export default AddBook;
-
-const FormContainer = styled.section`
-  width: 90%;
-  margin-inline: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  @media screen and (min-width: 768px) {
-    align-items: flex-start;
-  }
-`;
-
-const Title = styled.h2`
-  font-family: var(--montserrat);
-  font-size: 1rem;
-  font-weight: bold;
-  color: var(--mid-gray);
-  margin-bottom: 6px;
-`;
-
-const Form = styled.form`
-  width: 100%;
-  text-align: center;
-  margin-inline: auto;
-  margin-bottom: 1rem;
-  @media screen and (min-width: 768px) {
-    text-align: left;
-  }
-`;
-
-const Input = styled.input`
-  width: 90%;
-  border: 1px solid #e8e8e8;
-  padding: 2px 8px;
-  margin: 4px;
-  border-radius: 6px;
-  @media screen and (min-width: 768px) {
-    width: 30%;
-    padding: 8px 20px;
-  }
-`;
-
-const Button = styled.button`
-  outline: none;
-  border: none;
-  margin-inline: auto;
-  padding: 0.2rem 1rem;
-  background-color: var(--blue);
-  font-family: var(--roboto);
-  font-weight: bold;
-  font-size: 0.6rem;
-  border-radius: 3px;
-  color: white;
-  cursor: pointer;
-  transition: 0.3s ease;
-  &:hover {
-    transform: scale(1.1);
-  }
-  @media screen and (min-width: 768px) {
-    font-size: 0.9rem;
-    padding-inline: 3rem;
-  }
-`;
